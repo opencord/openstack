d8fa43d4693dfe4dea4a3178fd9ddc8dc547c648
Scott Baker <smbaker@gmail.com>
2016-08-16 16:41:08 -0700

    add config file for openstack synchronizer
    Change-Id: I78dde1377c0f23edc8a9c0255bfa80e304c9413b

--- xos/synchronizer/run.sh
+++ xos/synchronizer/run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python /opt/xos/synchronizers/openstack/xos-synchronizer.py
+
+export XOS_DIR=/opt/xos
+python $XOS_DIR/synchronizers/openstack/xos-synchronizer.py -C $XOS_DIR/synchronizers/openstack/openstack_synchronizer_config
