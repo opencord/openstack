af4ba42ddcab8ac1a2ac804b36556e6304d7c95e
Matteo Scandolo <teo@opennetworking.org>
2018-06-12 13:39:36 -0700

    [CORD-3022] Splitting config
    Change-Id: I157ca24ccf41b390e7c4cdb787d346a956f31b2f

--- xos/synchronizer/openstack-synchronizer.py
+++ xos/synchronizer/openstack-synchronizer.py
@@ -21,8 +21,13 @@ import sys
 sys.path.append('/opt/xos')
 from xosconfig import Config
 
-config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/openstack_config.yaml')
-Config.init(config_file, 'synchronizer-config-schema.yaml')
+base_config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/config.yaml')
+mounted_config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/mounted_config.yaml')
+
+if os.path.isfile(mounted_config_file):
+    Config.init(base_config_file, 'synchronizer-config-schema.yaml', mounted_config_file)
+else:
+    Config.init(base_config_file, 'synchronizer-config-schema.yaml')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xos.settings")
 from xos.logger import Logger, logging, logger
